aa94391c44841c818467a6d85506fe6c7f452ad4
Raymond Ho <chunkiat82@gmail.com>
2016-12-05 14:14:33 +0800

    fastforward and reset working

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,18 +1,25 @@
 import path from 'path';
-const { basename, dirname } = path;
+const {
+  basename,
+  dirname
+} = path;
 import git from 'git-node';
 import fs from 'fs-promise';
 require('colors');
 var jsdiff = require('diff');
 var debug = require('debug')('timedfile');
 
-const PATH_DELIMITER = '/';//path.delimiter;
+const PATH_DELIMITER = '/'; //path.delimiter;
 const FIXED_MESSAGE = 'Raymond Ho @ 2016';
 
 export default class TimedFile {
   constructor(options) {
 
-    const {fileFullPath, author, versionsPath } = options;
+    const {
+      fileFullPath,
+      author,
+      versionsPath
+    } = options;
 
     this.fileFullPath = fileFullPath;
     this.directory = dirname(this.fileFullPath);
@@ -20,34 +27,40 @@ export default class TimedFile {
     this.repoPath = versionsPath || this.directory;
     this.headCommitFile = [this.repoPath, `${this.filename}.commit`].join(PATH_DELIMITER);
     this.repo = git.repo(this.repoPath);
+    this.rolls = [];
     this.tree = {};
     try {
       this.commitHash = fs.readFileSync(this.headCommitFile).toString();
     } catch (e) {
       this.commitHash = null;
     }
-
     debug('constructor - this.commitHash = %s', this.commitHash);
 
-    this.rolls = [];
   }
 
-  /* sample commit
-      var commit1 = {
-        author
-        contents: "# This is a test Repo\n\nIt's generated entirely by JavaScript\n"
-      };
-  */
   _saveBlob = (commit) => {
     const that = this;
 
-    const { filename, repo, tree } = that;
+    const {
+      filename,
+      repo,
+      tree
+    } = that;
 
-    const { contents, author } = commit;
+    const {
+      contents,
+      author
+    } = commit;
 
     const message = FIXED_MESSAGE;
 
-    const gitCommit = { author, committer: author, filename, contents, message };
+    const gitCommit = {
+      author,
+      committer: author,
+      filename,
+      contents,
+      message
+    };
 
     return new Promise((resolve, reject) => {
       repo.saveAs("blob", contents, function (err, contentsHash) {
@@ -61,21 +74,35 @@ export default class TimedFile {
     });
   }
 
-  _saveAsCommit = async (commit) => {
+  _saveAsCommit = async(commit) => {
 
     const that = this;
 
-    const { filename, tree, repo, repoPath, commitHash  } = that;
+    const {
+      filename,
+      tree,
+      repo,
+      repoPath,
+      commitHash
+    } = that;
 
-    // const commitHash = await that._loadCommitHead();
-
-    const { contents, author } = commit;
+    const {
+      contents,
+      author
+    } = commit;
 
     const message = FIXED_MESSAGE;
 
     debug('_saveAsCommit - commitHash = %s', commitHash);
 
-    const gitCommit = { author, parent: commitHash, committer: author, filename, contents, message };
+    const gitCommit = {
+      author,
+      parent: commitHash,
+      committer: author,
+      filename,
+      contents,
+      message
+    };
 
     return new Promise((resolve, reject) => {
       //get a new parent
@@ -83,10 +110,12 @@ export default class TimedFile {
       repo.saveAs('tree', tree, function (err, treeHash) {
         if (err) return reject(err);
         debug('treeHash = %s', treeHash);
-        const treeCommit = Object.assign({}, gitCommit, { tree: treeHash });
+        const treeCommit = Object.assign({}, gitCommit, {
+          tree: treeHash
+        });
         if (commitHash === null) delete treeCommit.parent;
 
-        repo.saveAs("commit", treeCommit, async (err, commitHash) => {
+        repo.saveAs("commit", treeCommit, async(err, commitHash) => {
           if (err) return reject(err);
           await that._saveCommitHead(commitHash);
           resolve(commitHash);
@@ -98,7 +127,9 @@ export default class TimedFile {
   /* this loads the tree of files, not really needed for single file repo */
   _loadTree = (commitTreeHash) => {
     const that = this;
-    const { repo } = that;
+    const {
+      repo
+    } = that;
 
     return new Promise((resolve, reject) => {
       // debug(`repo=${repo}`);
@@ -112,7 +143,9 @@ export default class TimedFile {
 
   _loadCommit = (commitHash) => {
     const that = this;
-    const { repo } = that;
+    const {
+      repo
+    } = that;
     return new Promise((resolve, reject) => {
       repo.loadAs('commit', commitHash, (err, commit) => {
         if (err) return reject(err);
@@ -123,7 +156,9 @@ export default class TimedFile {
 
   _load = (blobHash) => {
     const that = this;
-    const { repo } = that;
+    const {
+      repo
+    } = that;
     return new Promise((resolve, reject) => {
       repo.loadAs('text', blobHash, (err, text) => {
         if (err) return reject(err);
@@ -132,61 +167,47 @@ export default class TimedFile {
     });
   }
 
-  _saveCommitHead = async (commitHash) => {
+  _saveCommitHead = async(commitHash) => {
     const that = this;
-    const { headCommitFile } = that;
+    const {
+      headCommitFile
+    } = that;
     return await fs.writeFile(headCommitFile, commitHash);
   };
 
-  // _loadCommitHead = async () => {
-  //   const that = this;
-  //   const { headCommitFile } = that;
-  //   let commitHash = null;
-
-  //   try {
-  //     const readFile = await fs.readFileSync(headCommitFile);
-  //     commitHash = readFile.toString();
-  //     debug('commitHash = %s', commitHash || 'EMPTY');
-  //   } catch (err) {
-  //     debug('file not found for %s', headCommitFile);
-  //   }
-
-  //   return commitHash;
-  // }
-
-  save = async (author) => {
+  save = async(author) => {
     var that = this;
 
-    const { fileFullPath } = that;
+    const {
+      fileFullPath
+    } = that;
 
     try {
       const readFile = await fs.readFile(fileFullPath);
       const contents = readFile.toString();
-      const commit = { author, contents };
+      const commit = {
+        author,
+        contents
+      };
 
       const contentsHash = await that._saveBlob(commit);
       debug('save - contentsHash = %s', contentsHash);
       that.commitHash = await that._saveAsCommit(commit);
       debug('save - that.commitHash = %s', that.commitHash);
-      // const headCommit = await that._loadCommit(that.commitHash);
-      // debug('headCommitTree = %s', headCommit.tree);
-      // const loadTree = await that._loadTree(headCommit.tree);
-      // debug('loadTree = %s', loadTree);
-      // const text = await that._load(contentsHash)
-      // debug('text = %s', text);
 
     } catch (e) {
       debug('save - error %s', e);
       throw new Error(e);
-
     }
 
   }
 
-  diff = async () => {
+  diff = async() => {
     const that = this;
-    const { fileFullPath, commitHash } = that;
-    // const commitHash = await that._loadCommitHead();
+    const {
+      fileFullPath,
+      commitHash,
+    } = that;
 
     const readFile = await fs.readFile(fileFullPath);
     const currentText = readFile.toString();
@@ -204,17 +225,37 @@ export default class TimedFile {
     }
   }
 
-  fastforward = () => { }
+  fastforward = async() => {
+    const that = this;
+    const {
+      fileFullPath,
+      rolls
+    } = that;
+
+    const commit = rolls.pop();
+
+    if (commit) {
+      debug('fastforward - commit.tree - %s', commit.tree);      
+      const loadTreeDiff = await that._loadTree(commit.tree);
+      debug('rollback - loadTreeDiff[0].hash = %s', loadTreeDiff && loadTreeDiff[0].hash);
+      const loadText = await that._load(loadTreeDiff[0].hash);
+      await fs.writeFile(fileFullPath, loadText);
+    } else {
+      debug('Not existing commit found for fastforward');
+    }
+
+  }
 
   /* we have to check if file is saved , not done yet */
-  rollback = async () => {
+  rollback = async() => {
 
     //check if file here TBD
 
     const that = this;
-    const { fileFullPath, headCommitFile, commitHash } = that;
-
-    // const commitHash = await that._loadCommitHead();
+    const {
+      fileFullPath,
+      commitHash
+    } = that;
 
     if (commitHash) {
       let commit = await that._loadCommit(commitHash);
@@ -238,8 +279,26 @@ export default class TimedFile {
 
   }
 
-  reset = async () => {
+  reset = async() => {
+    const that = this;
 
+    try {
+      const commitHashBufffer = await fs.readFile(that.headCommitFile);
+      that.commitHash = commitHashBufffer.toString();
+    } catch (e) {
+      that.commitHash = null;
+    }
+
+    const { commitHash, fileFullPath } = that;
+
+    if (commitHash) {
+      const commit = await that._loadCommit(commitHash);
+      const loadTreeDiff = await that._loadTree(commit.tree);
+      const loadText = await that._load(loadTreeDiff[0].hash);
+      await fs.writeFile(fileFullPath, loadText);
+    } else {
+      debug('Not existing commit found for reset');
+    }
   }
 
 }
--- test/unit/index.js
+++ test/unit/index.js
@@ -1,93 +1,190 @@
 import fs from 'fs-promise';
 import path from 'path';
-import { assert } from 'chai';
+import {
+  assert
+} from 'chai';
 import TimedFile from '../../src/lib/index';
 import {
-    log
+  log
 } from 'mocha-logger';
-const PATH_DELIMITER = '/';//path.delimiter;
+const PATH_DELIMITER = '/'; //path.delimiter;
 const gitTestFolder = [__dirname, '..', 'testcases', 'git'].join(PATH_DELIMITER);
 const contentTestFolder = [__dirname, '..', 'testcases', 'content'].join(PATH_DELIMITER);
 const fileFullPath = [contentTestFolder, 'saveTest.js'].join(PATH_DELIMITER);
 
-describe('TimedFile', function() {
+describe('TimedFile', function () {
 
-    before(async () => {
-        await fs.createFile(fileFullPath);
-    });
+  before(async() => {
+    await fs.createFile(fileFullPath);
+  });
 
-    describe('Functionalities Present', function() {
-        it('Functionalities Present', function(done) {
-            const timedFile = new TimedFile({ fileFullPath: `${__dirname}/LICENSE`, versionsPath: `${gitTestFolder}` });
-            expect(timedFile).to.have.property('save');
-            expect(timedFile).to.have.property('diff');
-            expect(timedFile).to.have.property('fastforward');
-            expect(timedFile).to.have.property('rollback');
-            done();
-        });
+  describe('Functionalities Present', function () {
+    it('Functionalities Present', function (done) {
+      const timedFile = new TimedFile({
+        fileFullPath: `${__dirname}/LICENSE`,
+        versionsPath: `${gitTestFolder}`
+      });
+      expect(timedFile).to.have.property('save');
+      expect(timedFile).to.have.property('diff');
+      expect(timedFile).to.have.property('fastforward');
+      expect(timedFile).to.have.property('rollback');
+      done();
     });
+  });
 
-    describe('Save', function() {
-        it('Able to Save', async function() {
-            const author = { name: 'Raymond Ho', email: 'chunkiat82@gmail.com' };
-            const timedFile = new TimedFile({ fileFullPath, versionsPath: `${gitTestFolder}` });
-            await fs.writeFile(fileFullPath, 'Line 1\n');
-            await timedFile.save(author);
-            const jsDiffs = await timedFile.diff();
-            expect(jsDiffs).to.eql([{ value: 'Line 1\n', count: 7 }]);
-        });
+  describe('Save', function () {
+    it('Able to Save', async function () {
+      const author = {
+        name: 'Raymond Ho',
+        email: 'chunkiat82@gmail.com'
+      };
+      const timedFile = new TimedFile({
+        fileFullPath,
+        versionsPath: `${gitTestFolder}`
+      });
+      await fs.writeFile(fileFullPath, 'Line 1\n');
+      await timedFile.save(author);
+      const jsDiffs = await timedFile.diff();
+      expect(jsDiffs).to.eql([{
+        value: 'Line 1\n',
+        count: 7
+      }]);
     });
+  });
 
-    describe('Diff', function() {
-        it('Able to Diff When Loaded with Versions', async function() {
-            const author = { name: 'Raymond Ho', email: 'chunkiat82@gmail.com' };
-            await fs.appendFile(fileFullPath, 'Line 2\n');
-            const timedFile = new TimedFile({ fileFullPath, versionsPath: `${gitTestFolder}` });            
-            const jsDiffs = await timedFile.diff();
-            expect(jsDiffs).to.eql([{ count: 7, value: 'Line 1\n' },
-            { count: 7, added: true, removed: undefined, value: 'Line 2\n' }]);
-            
-        });
-    });
+  describe('Diff', function () {
+    it('Able to Diff When Loaded with Versions', async function () {
+      const author = {
+        name: 'Raymond Ho',
+        email: 'chunkiat82@gmail.com'
+      };
+      await fs.appendFile(fileFullPath, 'Line 2\n');
+      const timedFile = new TimedFile({
+        fileFullPath,
+        versionsPath: `${gitTestFolder}`
+      });
+      const jsDiffs = await timedFile.diff();
+      expect(jsDiffs).to.eql([{
+        count: 7,
+        value: 'Line 1\n'
+      }, {
+        count: 7,
+        added: true,
+        removed: undefined,
+        value: 'Line 2\n'
+      }]);
 
-    describe('Save', function() {
-        it('Able to Save When Loaded with Versions', async function() {
-            const author = { name: 'Raymond Ho', email: 'chunkiat82@gmail.com' };
-            const timedFile = new TimedFile({ fileFullPath, versionsPath: `${gitTestFolder}` });
-            await timedFile.save(author);
-            const jsDiffs = await timedFile.diff();
-            expect(jsDiffs).to.eql([{
-                count: 14,
-                value: 'Line 1\nLine 2\n'
-            }]);
-        
-        });
     });
+  });
 
-    describe('Rollback', function() {
-        it('Able to Rollback', async function() {
-            const author = { name: 'Raymond Ho', email: 'chunkiat82@gmail.com' };
-            const timedFile = new TimedFile({ fileFullPath, versionsPath: `${gitTestFolder}` });
-            const beforeRollback = await fs.readFile(fileFullPath);
-            expect(beforeRollback.toString()).to.equal('Line 1\nLine 2\n');
-            const jsDiffs = await timedFile.diff();
-            await timedFile.rollback();
-            const afterRollback = await fs.readFile(fileFullPath);
-            expect(afterRollback.toString()).to.equal('Line 1\n');
-            expect(timedFile.rolls.length).to.equal(1);
-        });
-    });
+  describe('Save', function () {
+    it('Able to Save When Loaded with Versions', async function () {
+      const author = {
+        name: 'Raymond Ho',
+        email: 'chunkiat82@gmail.com'
+      };
+      const timedFile = new TimedFile({
+        fileFullPath,
+        versionsPath: `${gitTestFolder}`
+      });
+      await timedFile.save(author);
+      const jsDiffs = await timedFile.diff();
+      expect(jsDiffs).to.eql([{
+        count: 14,
+        value: 'Line 1\nLine 2\n'
+      }]);
 
-    describe('FastForward', function() {
-        it('Able to FastForward', async function() {
-            const author = { name: 'Raymond Ho', email: 'chunkiat82@gmail.com' };
-            const timedFile = new TimedFile({ fileFullPath, versionsPath: `${gitTestFolder}` });
-        });
     });
-    
-    after('Tear Down', async () => {
-        await fs.remove(contentTestFolder);
-        await fs.remove(gitTestFolder);
+  });
+
+  describe('Rollback', function () {
+    it('Able to Rollback', async function () {
+      const author = {
+        name: 'Raymond Ho',
+        email: 'chunkiat82@gmail.com'
+      };
+      const timedFile = new TimedFile({
+        fileFullPath,
+        versionsPath: `${gitTestFolder}`
+      });
+      const beforeRollback = await fs.readFile(fileFullPath);
+      expect(beforeRollback.toString()).to.equal('Line 1\nLine 2\n');
+      const jsDiffs = await timedFile.diff();
+      await timedFile.rollback();
+      const afterRollback = await fs.readFile(fileFullPath);
+      expect(afterRollback.toString()).to.equal('Line 1\n');
+      expect(timedFile.rolls.length).to.equal(1);
+      await timedFile.reset();
     });
+  });
+
+  describe('FastForward', function () {
+    it('Nothing to Fast Forward', async function () {
+      const author = {
+        name: 'Raymond Ho',
+        email: 'chunkiat82@gmail.com'
+      };
+      const timedFile = new TimedFile({
+        fileFullPath,
+        versionsPath: `${gitTestFolder}`
+      });
+      const beforeRollback = await fs.readFile(fileFullPath);
+      expect(beforeRollback.toString()).to.equal('Line 1\nLine 2\n');
+      expect(timedFile.rolls.length).to.equal(0);
+      await timedFile.fastforward();
+      const afterFastForward = await fs.readFile(fileFullPath);
+      expect(afterFastForward.toString()).to.equal('Line 1\nLine 2\n');
+      expect(timedFile.rolls.length).to.equal(0);
+    });
+  });
+
+  describe('FastForward', function () {
+    it('Able to FastForward', async function () {
+      const author = {
+        name: 'Raymond Ho',
+        email: 'chunkiat82@gmail.com'
+      };
+      const timedFile = new TimedFile({
+        fileFullPath,
+        versionsPath: `${gitTestFolder}`
+      });
+      const beforeRollback = await fs.readFile(fileFullPath);
+      expect(beforeRollback.toString()).to.equal('Line 1\nLine 2\n');
+      await timedFile.rollback();
+      const afterRollback = await fs.readFile(fileFullPath);
+      expect(afterRollback.toString()).to.equal('Line 1\n');
+      expect(timedFile.rolls.length).to.equal(1);
+      await timedFile.fastforward();
+      const afterFastForward = await fs.readFile(fileFullPath);
+      expect(afterFastForward.toString()).to.equal('Line 1\nLine 2\n');
+      expect(timedFile.rolls.length).to.equal(0);
+    });
+  });
+
+  describe('Reset', function () {
+    it('Able to Reset', async function () {
+      const author = {
+        name: 'Raymond Ho',
+        email: 'chunkiat82@gmail.com'
+      };
+      const timedFile = new TimedFile({
+        fileFullPath,
+        versionsPath: `${gitTestFolder}`
+      });
+      await fs.appendFile(fileFullPath, 'Line 3\n');
+      const beforeReset = await fs.readFile(fileFullPath);
+      expect(beforeReset.toString()).to.equal('Line 1\nLine 2\nLine 3\n');
+      expect(timedFile.rolls.length).to.equal(0);
+      await timedFile.reset();
+      const afterReset = await fs.readFile(fileFullPath);
+      expect(afterReset.toString()).to.equal('Line 1\nLine 2\n');
+      expect(timedFile.rolls.length).to.equal(0);
+    });
+  });
+
+  after('Tear Down', async() => {
+    await fs.remove(contentTestFolder);
+    await fs.remove(gitTestFolder);
+  });
 
 });
